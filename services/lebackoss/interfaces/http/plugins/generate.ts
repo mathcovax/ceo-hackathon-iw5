a5e4383156fd025511dc01bd8d77d8141056ad2e
ZeRiix <willflo46@gmail.com>
2025-06-25 17:13:19 +0200

    add duplo generate SubmissionData and SubmissionField

--- services/lebackoss/interfaces/http/plugins/generate.ts
+++ services/lebackoss/interfaces/http/plugins/generate.ts
@@ -2,7 +2,10 @@ import "@duplojs/types-codegen";
 import { submissionFieldObjecter } from "@business/domains/common/submissionField";
 import { PrestationSheet } from "@business/domains/entities/prestationSheet";
 import { prestationSheetSchema } from "../schemas/prestationSheet";
+import { submissionDataObjecter } from "@business/domains/common/submissionData";
 
 submissionFieldObjecter.zodSchema._zttIdentifier = "SubmissionField";
 PrestationSheet.modeObjecter.zodSchema._zttIdentifier = "PrestationSheetModeObjecter";
 prestationSheetSchema._zttIdentifier = "PrestationSheet";
+submissionDataObjecter.zodSchema._zttIdentifier = "SubmissionData";
+submissionFieldObjecter.zodSchema._zttIdentifier = "SubmissionField";
