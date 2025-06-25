32ce8b341e746733fe255d7d6ba9c8748e7d8ab8
ZeRiix <willflo46@gmail.com>
2025-06-25 09:58:14 +0200

    fix iconMapper list

--- libs/design-system/src/components/ui/icon/index.ts
+++ libs/design-system/src/components/ui/icon/index.ts
@@ -2,12 +2,16 @@ import {
 	mdiClose,
 	mdiAccount,
 	mdiRobot,
+	mdiHome,
+	mdiPlus,
 } from "@mdi/js";
 
 export const iconsMapper = {
 	close: mdiClose,
 	account: mdiAccount,
 	robot: mdiRobot,
+	home: mdiHome,
+	plus: mdiPlus,
 };
 
 export const iconSizeMapper = {
